5cc0a67830046f3f1cb925522f90c1c98d03c6d6
李杨 <1711467488@qq.com>
2018-01-29 23:30:58 +0800

    no message

--- src/components/MainLayout/Side/menu.js
+++ src/components/MainLayout/Side/menu.js
@@ -1,1 +1,12 @@
-export default []
+export default [
+  {
+    label: '测试',
+    value: 'test',
+    icon: 'el-icon-menu'
+  },
+  {
+    label: '测试2',
+    value: 'test2',
+    icon: 'el-icon-menu'
+  }
+]

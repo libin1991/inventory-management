e3b08ad056804c8ae5fc65fb700645c380ccffdf
李杨 <1711467488@qq.com>
2018-01-30 18:10:44 +0800

    no message

--- src/page/business/in/index.vue
+++ src/page/business/in/index.vue
@@ -9,6 +9,8 @@
 </template>
 
 <script>
+import low from 'lowdb'
+import LocalStorage from 'lowdb/adapters/LocalStorage'
 export default {
   data () {
     return {
@@ -22,6 +24,16 @@ export default {
         ]
       }
     }
+  },
+  mounted () {
+    const adapter = new LocalStorage('db2')
+    const db = low(adapter)
+    db.defaults({ posts: [] })
+      .write()
+    // Data is automatically saved to localStorage
+    db.get('posts')
+      .push({ title: 'lowdb' })
+      .write()
   },
   methods: {
     submitForm (formName) {

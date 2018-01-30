3e7598d93956bc141eb196d20eae7714158ee284
李杨 <1711467488@qq.com>
2018-01-30 20:29:12 +0800

    no message

--- src/mixins/db.js
+++ src/mixins/db.js
@@ -1,13 +1,20 @@
 export default {
   data () {
     return {
+      // 商品列表
       projects: []
     }
   },
   mounted () {
-    this.getProjects()
+    // 从数据库重新获得数据
+    this.reloadDataFromDB()
   },
   methods: {
+    // 从数据库重新获得数据
+    reloadDataFromDB () {
+      this.getProjects()
+    },
+    // 从数据库获得商品列表
     getProjects () {
       this.projects = this.$db
         .get('projects')

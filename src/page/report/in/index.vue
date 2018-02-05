150342301582a57366212fac88067af7d3208b99
李杨 <1711467488@qq.com>
2018-02-05 18:37:18 +0800

    no message

--- src/page/report/in/index.vue
+++ src/page/report/in/index.vue
@@ -1,5 +1,63 @@
 <template>
   <Container>
-    Report
+    <el-date-picker
+      v-model="date"
+      type="datetimerange"
+      :picker-options="pickerOptions2"
+      range-separator="至"
+      start-placeholder="开始日期"
+      end-placeholder="结束日期"
+      align="right">
+    </el-date-picker>
+    {{vuexHistoryInFilter}}
   </Container>
 </template>
+
+<script>
+import moment from 'moment'
+import vuex from '@/mixins/vuex.js'
+export default {
+  mixins: [
+    vuex
+  ],
+  data () {
+    return {
+      pickerOptions2: {
+        shortcuts: [{
+          text: '最近一周',
+          onClick(picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近一个月',
+          onClick(picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近三个月',
+          onClick(picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+            picker.$emit('pick', [start, end])
+          }
+        }]
+      },
+      date: [new Date(2000, 10, 10, 10, 10), new Date(2000, 10, 11, 10, 10)]
+    }
+  },
+  computed: {
+    vuexHistoryInFilter () {
+      return this.vuexHistoryIn.map(e => {
+        return moment(e.date).format('YYYY MM DD')
+      })
+    }
+  }
+}
+</script>

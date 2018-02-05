1b0506962f59870534568d85ab23a04eaf8d3415
李杨 <1711467488@qq.com>
2018-02-05 23:43:57 +0800

    no message

--- src/page/history/in/index.vue
+++ src/page/history/in/index.vue
@@ -5,14 +5,10 @@
 </template>
 
 <script>
-import moment from 'moment'
 import vuex from '@/mixins/vuex.js'
-import dict from '@/mixins/dict.js'
-moment.locale('zh-cn')
 export default {
   mixins: [
-    vuex,
-    dict
+    vuex
   ]
 }
 </script>
